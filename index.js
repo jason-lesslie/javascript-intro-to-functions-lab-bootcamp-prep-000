90c97f79a60ce255f04edef52420ea2331e742f9
Jason Lesslie <jason.lesslie@gmail.com>
2018-01-13 18:12:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ function logShout(string) {
   console.log(string.toUpperCase())
   return string.toUpperCase()
 }
-
 function logWhisper(string) {
   console.log(string.toLowerCase())
+  return string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  var strResponse
+  if (string === "hello"){
+    strResponse = "I can't hear you!"
+  } else if (string === "HELLO") {
+    strResponse = "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    strResponse = "I love you, too."
+  }
   return string.toLowerCase()
 }
